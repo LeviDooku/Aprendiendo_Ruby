738c426467150271814c3389089326f08ffa2fbc
LeviDooku <pedrovelascosantana@gmail.com>
2023-05-21 21:06:23 +0200

    Creo una carpeta pa organizar las cosas

---
+++ Entrada_salida_de_datos/imprime_mensaje.rb
@@ -0,0 +1,8 @@
+#Programa que imprime un mensaje introducido por el usuario en la terminal
+
+def imprime_mensaje(mensaje = "Hello World")
+	puts "Hola #{mensaje}"
+end 
+
+
+imprime_mensaje("ejemplo_nombre")
