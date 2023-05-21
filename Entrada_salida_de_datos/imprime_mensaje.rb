5f454aebdf84a542a167f3bef4bbca76c3723074
LeviDooku <pedrovelascosantana@gmail.com>
2023-05-21 22:02:56 +0200

    Actualizado el readme

--- Entrada_salida_de_datos/imprime_mensaje.rb
+++ Entrada_salida_de_datos/imprime_mensaje.rb
@@ -3,10 +3,10 @@
 #Declaramos una función con un parámetro "nombre", que por defecto es = "World"
 
 def imprime_nombre(nombre = "World")
-	puts "Hola #{nombre}"
+	puts "Hello #{nombre}"
 end
 
-#Si se le pasa un parámetro a la función imprime "Hola Pepote"
+#Si se le pasa un parámetro a la función imprime "Hello Pepote"
 
 imprime_nombre("Pepote")
 
@@ -22,10 +22,10 @@ imprime_nombre #=> Es válido escribir "imprime_nombre()"
 #nos aseguramos de que la salida siempre comienze con una mayúscula
 
 def imprime_nombre_mayus(nombre = "World")
-	puts "Hola #{nombre.capitalize}" 
+	puts "Hello #{nombre.capitalize}" 
 end
 
 puts "\n"
 
-imprime_nombre_mayus("pepote") #=> La salida será "Hola Pepote"
+imprime_nombre_mayus("pepote") #=> La salida será "Hello Pepote"
 
--- README.md
+++ README.md
@@ -5,4 +5,7 @@ Repositorio de pequeños y simples programas en Ruby.
 Ruby es un lenguaje interpretado, por ello no se necesita compilador (a tomar por saco los makefiles por fin), para ejecutar un programa en la terminal se usa el comando `ruby nombre_del_archivo.rb`
 
 Lista de programas:  
-- `hola_mundo.rb`: imprimir un "Hola mundo" en la terminal
+- De la carpeta `Entrada_salida_de_datos`
+	- `hola_mundo.rb`: imprimir un "Hola mundo" en la terminal.  
+	- `imprime_mensaje.rb`: imprime un mensaje usando una función a la que se le pasa un parámetro, si no se le pasa, imprime uno por defecto.  
+	- `imprime_mensaje_class.rb`: introducción a las clases, se crea con distintos métodos para imprimir distintos mensajes.
