5ed3850a0e528f8bb0368b86257bfc9073f40617
LeviDooku <pedrovelascosantana@gmail.com>
2023-05-21 20:51:23 +0200

    Subido programa q declara una funcion que imprime un mensaje

---
+++ hola_mundo.rb
@@ -0,0 +1,11 @@
+#Programa que imprime un "Hello Word" en la terminal
+
+#Creamos una función "hello_world"
+
+def hola_mundo #=> Sobre la sintaxis: el nombre de las funciones siempre debe empezar por minúscula
+	puts "Hola mundo"
+end 
+
+#Y la llamamos de esta forma
+
+hola_mundo
